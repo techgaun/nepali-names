7669509b182648cd3494e4290b252144ed894bf0
techgaun <coolsamar207@gmail.com>
2017-07-02 12:42:50 -0500

    add new names

--- extractor.sh
+++ extractor.sh
@@ -2,14 +2,14 @@
 
 # for girl: ./extractor.sh Girl
 gender=${1:-Boy}
-tmpfile="/tmp/names.txt"
+tmpfile="/tmp/${gender}.txt"
 rm -f "${tmpfile}"
 
 for x in {A..Z}; do
   url="http://www.nepaliname.com/babyname/startingWith/${x}-${gender}"
   response=$(curl -sS "${url}")
   names=$(pup 'a[href^="/meaning/of"] attr{href}' <<< "${response}" | cut -d"/" -f4 | awk '{print $1}' | sed \
-  's/[^a-zA-Z]//g' | tr 'A-Z' 'a-z' | sort | uniq)
+  's/[^a-zA-Z]//g' | tr 'A-Z' 'a-z' | sed 's/./\u&/' | sort | uniq)
   echo "${names}" >> "${tmpfile}"
 done
 
